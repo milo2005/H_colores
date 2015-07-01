72e89b1f49025afb17a6d884efc36b0619159d98
Dario Chamorro <dario.chamorro.v@gmail.com>
2015-07-01 10:59:29 -0500

    Lista de colores con arraylist y un template

--- app/src/main/java/unicauca/movil/holamundo/coleccionesii/MainActivity.java
+++ app/src/main/java/unicauca/movil/holamundo/coleccionesii/MainActivity.java
@@ -4,12 +4,15 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 
 public class MainActivity extends ActionBarActivity {
 
     ListView list;
+    String colores[];
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,12 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         list = (ListView) findViewById(R.id.list);
+
+        colores = getResources().getStringArray(R.array.colores);
+
+        adapter = new ArrayAdapter<String>(this,R.layout.template_list,R.id.txt,colores);
+
+        list.setAdapter(adapter);
 
     }
 
